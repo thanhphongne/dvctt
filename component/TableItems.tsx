4b7c36bbee79ad31ae556190d30312e362da8315
thanhphongne <btphongc3tl@gmail.com>
2022-11-09 13:56:08 +0700

    initial commit

--- component/TableItems.tsx
+++ component/TableItems.tsx
@@ -9,15 +9,23 @@ type Props = {
 };
 
 const TableItem = ({ tthc, handleViewPDF, selectedId }: Props) => {
-    const active = tthc.id === selectedId ? 'gray.6' : ''
+    const active = tthc.id === selectedId ? '#CBD0D5' : '';
+    const isDisable = tthc.id === selectedId ? true : false;
+
     return (
-        <tr>
+        <tr
+            style={{
+                backgroundColor: active,
+            }}
+        >
             <td>{tthc.id}</td>
             <td>{tthc.title}</td>
             <td>{tthc.createAt}</td>
             <td>{tthc.author}</td>
             <td>
-                <Button onClick={() => handleViewPDF(tthc)} color={active}>Xem</Button>
+                <Button onClick={() => handleViewPDF(tthc)} disabled={isDisable}>
+                    Xem
+                </Button>
             </td>
         </tr>
     );
